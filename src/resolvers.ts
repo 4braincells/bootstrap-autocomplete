57af1deda76c3c34ca0ac5e04edfa2855136df91
stefan <stefan@4braincells.de>
2024-09-04 16:09:54 +0200

    #147 fix "fail" callback in ressolvers.ts not to throw exception when undefined

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,89 +1,89 @@
 export class BaseResolver {
-  protected results: any[];
+    protected results: any[];
 
-  protected _settings: any;
+    protected _settings: any;
 
-  constructor(options: any) {
-    this._settings = $.extend(true, {}, this.getDefaults(), options);
-  }
+    constructor(options: any) {
+        this._settings = $.extend(true, {}, this.getDefaults(), options);
+    }
 
-  protected getDefaults(): {} {
-    return {};
-  }
+    protected getDefaults(): {} {
+        return {};
+    }
 
-  protected getResults(limit?: number, start?: number, end?: number): any[] {
+    protected getResults(limit?: number, start?: number, end?: number): any[] {
 
-    return this.results;
-  }
+        return this.results;
+    }
 
-  public search(q: string, cbk: (results: any[]) => void): void {
-    cbk(this.getResults());
-  }
+    public search(q: string, cbk: (results: any[]) => void): void {
+        cbk(this.getResults());
+    }
 
 }
 
 // tslint:disable-next-line: max-classes-per-file
 export class AjaxResolver extends BaseResolver {
-  protected jqXHR: JQueryXHR;
-  protected requestTID: number;
+    protected jqXHR: JQueryXHR;
+    protected requestTID: number;
 
-  constructor(options: any) {
-    super(options);
+    constructor(options: any) {
+        super(options);
 
-    // console.log('resolver settings', this._settings);
-  }
-
-  protected getDefaults(): {} {
-    return {
-      url: '',
-      method: 'get',
-      queryKey: 'q',
-      extraData: {},
-      timeout: undefined,
-      requestThrottling: 500
-    };
-  }
-
-  public search(q: string, cbk: (results: any[]) => void): void {
-    if (this.jqXHR != null) {
-      this.jqXHR.abort();
+        // console.log('resolver settings', this._settings);
     }
 
-    const data: {
-      [key: string]: any
-    } = {};
-    data[this._settings.queryKey] = q;
-    $.extend(data, this._settings.extraData);
-
-    // request throttling
-    if (this.requestTID) {
-      window.clearTimeout(this.requestTID);
+    protected getDefaults(): {} {
+        return {
+            url: '',
+            method: 'get',
+            queryKey: 'q',
+            extraData: {},
+            timeout: undefined,
+            requestThrottling: 500
+        };
     }
-    this.requestTID = window.setTimeout(() => {
-      this.jqXHR = $.ajax(
-        this._settings.url,
-        {
-          method: this._settings.method,
-          data,
-          timeout: this._settings.timeout
+
+    public search(q: string, cbk: (results: any[]) => void): void {
+        if (this.jqXHR != null) {
+            this.jqXHR.abort();
         }
-      );
 
-      this.jqXHR.done((result) => {
-        cbk(result);
-      });
+        const data: {
+            [key: string]: any
+        } = {};
+        data[this._settings.queryKey] = q;
+        $.extend(data, this._settings.extraData);
 
-      this.jqXHR.fail((err) => {
-        // console.log(err);
-        // this._settings.fail && this._settings.fail(err);
-        this._settings?.fail(err);
-      });
+        // request throttling
+        if (this.requestTID) {
+            window.clearTimeout(this.requestTID);
+        }
+        this.requestTID = window.setTimeout(() => {
+            this.jqXHR = $.ajax(
+                this._settings.url,
+                {
+                    method: this._settings.method,
+                    data,
+                    timeout: this._settings.timeout
+                }
+            );
 
-      this.jqXHR.always(() => {
-        this.jqXHR = null;
-      });
+            this.jqXHR.done((result) => {
+                cbk(result);
+            });
 
-    }, this._settings.requestThrottling);
-  }
+            this.jqXHR.fail((err) => {
+                if (this._settings?.fail === undefined)
+                    console.error(err);
+                this._settings?.fail?.(err);
+            });
+
+            this.jqXHR.always(() => {
+                this.jqXHR = null;
+            });
+
+        }, this._settings.requestThrottling);
+    }
 
 }
